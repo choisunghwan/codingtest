cf17b8f4213af4ee0055f8b668d64dbd46ab9eac
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-07-18 21:24:20 +0900

    대표값

--- 코드 구현력 기르기/4. 대표값.py
+++ 코드 구현력 기르기/4. 대표값.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open("C:\\Users\\csh\\Desktop\\코딩테스트\\코드 구현력 기르기\\input.txt",'rt') 
+#sys.stdin = open("C:\\Users\\csh\\Desktop\\코딩테스트\\코드 구현력 기르기\\input.txt",'rt') 
 
 n = int(input())
 a = list(map(int, input().split())) # N명의 학생의 수학 성적 입력받음 
