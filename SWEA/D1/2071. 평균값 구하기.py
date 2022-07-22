54e82171b9049219b99a5b67f20c52d9b2a3745f
unknown <dute7570@naver.com>
2022-07-22 13:44:33 +0900

    update

---
+++ SWEA/D1/2071. 평균값 구하기.py
@@ -0,0 +1,11 @@
+#import sys
+#sys.stdin = open("C:\\Users\\c6_112_26\\codingtest\\SWEA\\D1\\input.txt",'rt')
+
+
+T = int(input()) # 테스트케이스의 개수 T
+
+
+for testcount in range(T):
+    testcase=list(map(int,input().split))
+    for a in range(testcase):
+        print(a)
