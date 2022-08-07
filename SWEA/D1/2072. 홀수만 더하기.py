f65b3188440054e07fc0ff0a65092680d7f0b6ec
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-07 09:43:28 +0900

    다른 방식 풀이

--- SWEA/D1/2072. 홀수만 더하기.py
+++ SWEA/D1/2072. 홀수만 더하기.py
@@ -1,7 +1,18 @@
-T = int(input())
+import sys
+from tkinter import N
+sys.stdin=open("C:\\Users\\csh\\Desktop\\코딩테스트\\SWEA\\D1\\input.txt",'rt')
 
-for testcount in range(T) :
-    nums = map(int,input().split())
+T = int(input()) # 테스트 케이스의 개수 T
 
-    res = sum(n for n in nums if n%2==1)
-    print( f"#{testcount+1} {res}" )
+
+for testcount in range(T) : #테스트 케이스 수만큼
+    nums = list(map(int,input().split())) # 10개의 수 입력
+    res = 0
+    for i in nums:
+        if i % 2 ==1:
+            res +=i
+
+    print('#%d %d' % (testcount+1, res))
+
+#res = sum(n for n in nums if n%2==1) 
+#print( f"#{testcount} {res}" )
