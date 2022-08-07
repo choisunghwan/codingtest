90ae22f3f6fbb1b60918bfb84647d62cc7fdaa19
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-07 10:34:54 +0900

    평균값 구하기

--- SWEA/D1/2071. 평균값 구하기.py
+++ SWEA/D1/2071. 평균값 구하기.py
@@ -1,11 +1,13 @@
-#import sys
-#sys.stdin = open("C:\\Users\\c6_112_26\\codingtest\\SWEA\\D1\\input.txt",'rt')
+import sys
+import math
+sys.stdin = open("C:\\Users\\csh\\Desktop\\코딩테스트\\SWEA\\D1\\input.txt", 'rt')
 
 
-T = int(input()) # 테스트케이스의 개수 T
+T = int(input())  # 테스트케이스의 개수 T
+for testcount in range(1, T+1):  # 테스트 케이스 반복
+    res = 0
+    nums = list(map(int, input().split()))  # 숫자 입력받음.
 
-
-for testcount in range(T):
-    testcase=list(map(int,input().split))
-    for a in range(testcase):
-        print(a)
+    for i in nums: #입력 받은 수를 하나씩 i에 넣으면서 반복
+        res += i #i를 res에 누적 합
+    print("#%d %d" % (testcount, round(res/len(nums))))
