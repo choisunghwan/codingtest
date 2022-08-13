29d9867c9ae5760d439a35a4303df1ef9b4cfb97
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-13 16:23:40 +0900

    결정알고리즘(랜선자르기)

---
+++ 이분탐색(결정알고리즘)&그리디/2. 랜선자르기(결정알고리즘).py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin=open("C:\\Users\\csh\\Desktop\\코딩테스트\\이분탐색(결정알고리즘)&그리디\\input.txt",'rt') #read text :rt
+
+def Count(len):
+    cnt = 0
+    for x in line:
+        cnt +=(x//len)
+
+k, n = map(int,input().split()) # 이미 가지고 있는 랜선의 개수 k , 필요한 랜선의 개수 n
+line = []
+les = 0
+largest = 0 
+for i in range(k):
+    tmp = int(input())
+    line.append(tmp)
+    largest = max(largest, tmp)
+lt = 1
+rt = largest
+
+while lt < rt:
+    mid = (lt+rt)//2
+    if Count(mid) >= n:
