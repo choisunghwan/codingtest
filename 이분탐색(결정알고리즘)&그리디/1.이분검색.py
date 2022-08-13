83663e62cbb95d1be6bb65a8ea95904b6b6c627a
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-13 15:50:19 +0900

    이분 탐색

--- 이분탐색(결정알고리즘)&그리디/1.이분검색.py
+++ 이분탐색(결정알고리즘)&그리디/1.이분검색.py
@@ -1,7 +1,20 @@
 import sys
 sys.stdin=open("C:\\Users\\csh\\Desktop\\코딩테스트\\이분탐색(결정알고리즘)&그리디\\input.txt",'rt') #read text :rt
 
-n, m = int(input())
+n, m = map(int,input().split())
 nums = list(map(int,input().split()))
+nums.sort() #숫자들을 오름차순으로 정렬시킨다.
+lt = 0  # 맨 왼쪽 값을 0으로 
+rt = n-1 # 맨 오른쪽 값을 n-1 으로 (why?: 인덱스값은 0부터 시작하기때문에)
+
+while lt <= rt:
+    mid = (lt + rt)// 2
+    if nums[mid] == m:
+        print (mid+1)
+        break
+    elif nums[mid] > m:
+        rt = mid - 1 
+    else:
+        lt = mid + 1
 
 
