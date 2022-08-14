3c89606e3e89e44eb9c6ed04222a94e70836dd02
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-14 14:49:54 +0900

    랜선자르기

--- 이분탐색(결정알고리즘)&그리디/2. 랜선자르기(결정알고리즘).py
+++ 이분탐색(결정알고리즘)&그리디/2. 랜선자르기(결정알고리즘).py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin=open("C:\\Users\\csh\\Desktop\\코딩테스트\\이분탐색(결정알고리즘)&그리디\\input.txt",'rt') #read text :rt
+#sys.stdin=open("C:\\Users\\csh\\Desktop\\코딩테스트\\이분탐색(결정알고리즘)&그리디\\input.txt",'rt') #read text :rt
 
 def Count(len):
     cnt = 0
