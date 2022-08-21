feefec727668eac70c6bbe34d03b6a3dc09274d2
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-21 22:18:03 +0900

    씨름선수(그리디)복습

--- 이분탐색(결정알고리즘)&그리디/6.씨름선수(그리디).py
+++ 이분탐색(결정알고리즘)&그리디/6.씨름선수(그리디).py
@@ -5,16 +5,17 @@ n = int(input()) #지원자 수
 
 body= []
 
-for i in range(n): #5번 반복한다.
-    h,w = map(int,input().split()) #키, 몸무게 
-    body.append((h,w))
-body.sort(reverse=True) #내림차순 정렬
+for i in range(n): #지원자 수(n)번 반복한다.
+    h,w = map(int,input().split()) #키, 몸무게를 입력받아서
+    body.append((h,w)) # 튜플 형태로 키(h) 와 몸무게(w)를 body리스트에 추가해준다.
+
+body.sort(reverse=True) #내림차순 정렬  | 오름차순 : body.sort()
 
 largest = 0
 cnt = 0
 
-for x , y in body:
-    if y > largest:
-        largest = y
-        cnt +=1
-print(cnt)
+for x , y in body: #x:키 y:몸무게
+    if y > largest: #몸무게y가 최고 값(largest)보다 크다면
+        largest = y #저장
+        cnt +=1 # cnt 1증가
+print(cnt) # 선발된 인원 출력
