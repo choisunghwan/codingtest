ca97b56e5a3628fc4c3a104812303a822f1d5078
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-08-31 10:05:16 +0900

    2046 스탬프 찍기

---
+++ SWEA/D1/2046. 스탬프 찍기.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin=open("C:\\Users\\csh\\Documents\\코딩테스트\\SWEA\\D1\\input.txt",'rt')
+
+a = int(input()) # 주어진 숫자 입력(3)
+
+for _ in range(a): # a(3)만큼 반복
+    print("#", end='')
+
