52e04f9ab3d9a1547c7e7cf7f0f7a6e144ace7ba
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-09-19 23:29:21 +0900

    5. 공주구하기(큐)

---
+++ 자료구조활용(스택,큐,해쉬,힙)/5. 공주구하기(큐).py
@@ -0,0 +1,15 @@
+from re import L
+import sys
+sys.stdin=open("C:\\Users\\csh\\Documents\\코딩테스트\\자료구조활용(스택,큐,해쉬,힙)\\input.txt",'rt')
+
+n, k = map(int,input().split())
+dq=list(range(1,n+1))
+dq=deque(dq)
+while dq:
+    for _ in range(k-1):
+        cur=dq.popleft()
+        dq.append(Cur)
+    dq.popleft()
+    if len(dq)==1:
+        print(dq[0])
+        dq.popleft()
