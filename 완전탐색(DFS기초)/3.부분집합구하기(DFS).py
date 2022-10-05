9ffd41983f3bafbd4efb6aa68626736ccbc1c045
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-10-05 23:37:07 +0900

    부분집합구하기(DFS)

---
+++ 완전탐색(DFS기초)/3.부분집합구하기(DFS).py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin=open("C:\\Users\\csh\\Documents\\pycharmCodingTest\\input.txt",'rt')
+
+def DFS(v):
+    if v == n+1:
+        for i in range(1,n+1):
+            if ch[i] == 1:
+                print(i, end=' ')
+        print()
+    else:
+        ch[v] = 1
+        DFS(v+1)
+        ch[v] = 0
+        DFS(v+1)
+
+if __name__=="__main__":
+    n=int(input())
+    ch=[0]*(n+1)
+    DFS(1)
