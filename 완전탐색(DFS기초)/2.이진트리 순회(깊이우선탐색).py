fe22b164387babcfd9940e2b4822b0047aab37d2
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-10-06 23:34:54 +0900

    합이 같은 부분집합 DFS

--- 완전탐색(DFS기초)/2.이진트리 순회(깊이우선탐색).py
+++ 완전탐색(DFS기초)/2.이진트리 순회(깊이우선탐색).py
@@ -5,9 +5,10 @@ def DFS(v):
     if v>7:
         return
     else:
+        print(v, end=" ")
         DFS(v*2)
         DFS(v*2+1)
-        print(v, end=" ")
+        
 
 if __name__=="__main__":
     DFS(1)
