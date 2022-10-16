9c51b4573bf06545b76cae8ad14fb3af45fd1d4b
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-10-16 23:23:12 +0900

    update

--- 자료구조활용(스택,큐,해쉬,힙)/5. 공주구하기(큐).py
+++ 자료구조활용(스택,큐,해쉬,힙)/5. 공주구하기(큐).py
@@ -1,3 +1,5 @@
+from collections import deque
+import curses
 from re import L
 import sys
 sys.stdin=open("C:\\Users\\csh\\Documents\\코딩테스트\\자료구조활용(스택,큐,해쉬,힙)\\input.txt",'rt')
@@ -8,7 +10,7 @@ dq=deque(dq)
 while dq:
     for _ in range(k-1):
         cur=dq.popleft()
-        dq.append(Cur)
+        dq.append(curses)
     dq.popleft()
     if len(dq)==1:
         print(dq[0])
