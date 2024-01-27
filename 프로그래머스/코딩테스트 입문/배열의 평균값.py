7442604fbed431d785706915579b961e4b3c66d4
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2024-01-27 16:17:38 +0900

    배열의 평균값 -- sum() , len() 사용

--- 프로그래머스/코딩테스트 입문/배열의 평균값.py
+++ 프로그래머스/코딩테스트 입문/배열의 평균값.py
@@ -6,5 +6,6 @@ def solution(numbers):
     answer = sum(numbers) / len(numbers)
     return answer
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(solution(numbers))
+numbers = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],[89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]]
+for i in numbers:
+    print(solution(i))
