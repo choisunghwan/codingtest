9ea0d00ac40ae62e99384be1816f9e2ca74105d4
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2024-02-05 16:52:32 +0900

    배열 이용해서 풀기

---
+++ 프로그래머스/코딩테스트 입문/짝수 홀수 개수.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    answer = [0,0]
+
+    for num in num_list:
+        if num % 2 == 0:
+            answer[0] + 1
+        else:
+            answer[1] + 1
+    
+    return answer
+
+
+# def solution(num_list):
+#     answer = [0,0]
+#     for n in num_list:
+#         answer[n%2]+=1
+#     return answer
