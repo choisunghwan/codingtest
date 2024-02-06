86e0b1fca41340887eb75fbd9893303d24ab70cb
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2024-02-06 10:15:55 +0900

    if elif 조건사용

---
+++ 프로그래머스/코딩테스트 입문/각도기.py
@@ -0,0 +1,9 @@
+def solution(angle):
+    if angle > 0 and angle < 90:
+        return(1)
+    elif angle == 90:
+        return(2)
+    elif angle > 90 and angle <180:
+        return(3)
+    elif angle == 180:
+        return(4)
