36d76d006a874173cffac27726dd22a2acb35a1d
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2024-02-07 16:20:14 +0900

    팩토리얼 함수 만들어서 사용

---
+++ 프로그래머스/코딩테스트 입문/구슬을 나누는 경우의 수.py
@@ -0,0 +1,14 @@
+def fac(num):
+    a = 1
+    for i in range(1,num+1):
+        a *= i
+    return a
+
+def solution(balls, share):
+    answer = 0
+    n = fac(balls)
+    m = fac(share)
+    nm = fac(balls-share)
+    
+    answer = n / (nm*m)
+    return answer
