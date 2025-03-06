17e6b1dc6e90ec65ef37579303bfdf944657360a
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2025-03-06 23:26:56 +0900

    최대 힙

---
+++ BFS/특정거리의도시찾기.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/18352
+from collections import deque
+
+n , m , k, x = map(int,input().split())
+
+print(n,m,k,x)
+
+a,b = map(int,input().split())
+print(a,b)
---
+++ 자료구조활용(스택,큐,해쉬,힙)/11.최대힙.py
@@ -0,0 +1,24 @@
+# 6
+# 0
+# 5
+# 0
+# 2
+# 4
+# 0
+# -1
+import sys
+import heapq as hq
+sys.stdin=open("C:\\Users\\csh\\Documents\\코딩테스트\\자료구조활용(스택,큐,해쉬,힙)\\input.txt",'rt')
+
+a= []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    if n == 0:
+        if len(a) == 0:
+            print(-1)
+        else:
+            print(-hq.heappop(a))
+    else:
+        hq.heappush(a, -n)
